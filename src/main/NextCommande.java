79a65b69eb922b1686f64aa245097eed94449a71
UVSQ-21807940 <37400158+UVSQ-21807940@users.noreply.github.com>
2020-05-24 06:12:44 +0200

    Ajout commande

---
+++ src/main/NextCommande.java
@@ -0,0 +1,16 @@
+package main;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class NextCommande implements Commande {
+	 DrawingTUI re;
+	 
+
+     public NextCommande(DrawingTUI re) {
+  	   this.re=re;
+     }
+   public void execute() throws IOException, SQLException {
+	    re.NextCommande();
+}
+	
+}
